42013a59d65cf2bd238be59f7db4abc7d2454e26
Daniel Deziel <32435437+Deziel0495@users.noreply.github.com>
2017-10-26 13:33:41 -0300

    Update client.lua

--- TowScript/client.lua
+++ TowScript/client.lua
@@ -1,6 +1,11 @@
--- Created by Asser90, modified by Deziel0495 --
+-- Created by Asser90 - modified by Deziel0495 and IllusiveTea --
 
 local currentlyTowedVehicle = nil
+
+RegisterCommand("tow", function()
+	TriggerEvent("tow")
+end,restricted)
+
 
 RegisterNetEvent('tow')
 AddEventHandler('tow', function()
@@ -16,21 +21,34 @@ AddEventHandler('tow', function()
 		local coordA = GetEntityCoords(playerped, 1)
 		local coordB = GetOffsetFromEntityInWorldCoords(playerped, 0.0, 5.0, 0.0)
 		local targetVehicle = getVehicleInDirection(coordA, coordB)
+
 		
-		
+		Citizen.CreateThread(function()
+			while true do
+				Citizen.Wait(0)
+				local towmodel = GetHashKey('flatbed')
+				local isVehicleTow = IsVehicleModel(vehicle, towmodel)
+				roll = GetEntityRoll(GetVehiclePedIsIn(PlayerPedId(), true))
+				if IsEntityUpsidedown(GetVehiclePedIsIn(PlayerPedId(), true)) and isVehicleTow or roll > 70.0 or roll < -70.0 then
+					DetachEntity(currentlyTowedVehicle, false, false)
+					currentlyTowedVehicle = nil
+				end
+			end
+		end)
+
 		if currentlyTowedVehicle == nil then
 			if targetVehicle ~= 0 then
 				if not IsPedInAnyVehicle(playerped, true) then
-					if vehicle ~= targetVehicle then
+					if vehicle ~= targetVehicle and IsVehicleStopped(vehicle) then
 						AttachEntityToEntity(targetVehicle, vehicle, GetEntityBoneIndexByName(vehicle, 'bodyshell'), 0, -2.0, 1.0, 0, 0, 0, 1, 1, 0, 1, 0, 1)
 						currentlyTowedVehicle = targetVehicle
-						ShowNotification("Vehicle has been successfully ~g~ATTACHED ~s~to the flatbed.")
+						ShowNotification("Vehicle has been succesfully ~g~ATACHED ~s~to the flatbed.")
 					else
 						ShowNotification("~o~You cannot tow your own truck!")
 					end
 				end
 			end
-		else
+		elseif IsVehicleStopped(vehicle) then
 		DetachEntity(currentlyTowedVehicle, false, false)
 		local vehiclesCoords = GetOffsetFromEntityInWorldCoords(vehicle, 0.0, -10.0, 0.0)
 			SetEntityCoords(currentlyTowedVehicle, vehiclesCoords["x"], vehiclesCoords["y"], vehiclesCoords["z"], 1, 0, 0, 1)
