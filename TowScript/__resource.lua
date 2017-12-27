42cbf39c5d63df17ca0daf5686170ec9964bc863
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2017-12-27 15:38:30 +0100

    Many Updates (check descriptoin)
    __resource.lua changes:
    + Added resource manifest version to allow the latest natives to be used.
    
    client.lua changes:
    ```diff
    + Added a list where users can allow multiple flatbed models to be used.
    - Removed the `GetHashKey()` for vehicle models as it's no longer needed
    + Added 5 category toggles to disable the towing of certain vehicle types (boats, planes, helicopters, trains and vehicles inside the utility class (including all trailers, sadly also some other vehicles like "sadler")). Each type can be enabled/disabled by setting it's variable to either true/false.
    - Removed duplicate *local variables* (removed the "local" from already existing variables)
    + Added 2 functions. 1) to check for a valid tow truck. 2) check if the target vehicle is a utility class vehicle (trailer).
    + Added a lot more notifications to notify the user if they are trying to use the /tow command in a wrong way, and tell them _what_ they're doing wrong instead of just not giving any feedback.
    NOTE: The messages probably need some spelling/grammar checks...
    + Fixed incorrect indentation for some lines.
    + Updated the ShowNotification function to use updated natives.
    ```
    There might be some other changes that I have missed, just double check the source code before merging please.
    
    I have tested this _a lot_ on my own server and it seems to all work perfectly fine. Haven't had any issues while testing. Let me know if there's something broken.

--- TowScript/__resource.lua
+++ TowScript/__resource.lua
@@ -1,2 +1,4 @@
--- Created by Asser90 - modified by Deziel0495 and IllusiveTea --
+-- Created by Asser90 - modified by Deziel0495 and IllusiveTea - further modified by Vespura --
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
 client_script 'client.lua'
--- TowScript/client.lua
+++ TowScript/client.lua
@@ -1,4 +1,15 @@
--- Created by Asser90 - modified by Deziel0495 and IllusiveTea --
+-- Created by Asser90 - modified by Deziel0495 and IllusiveTea - further modified by Vespura --
+
+-- These vehicles will be registered as "allowed/valid" tow trucks.
+local allowedTowModels = { 
+    'flatbed',
+    'flatbed2',
+}
+local allowTowingBoats = false -- Set to true if you want to be able to tow boats.
+local allowTowingPlanes = false -- Set to true if you want to be able to tow planes.
+local allowTowingHelicopters = false -- Set to true if you want to be able to tow helicopters.
+local allowTowingTrains = false -- Set to true if you want to be able to tow trains.
+local allowTowingTrailers = false -- Disables trailers. NOTE: THIS ALSO DISABLES: AIRTUG, TOWTRUCK, SADLER, ANY OTHER VEHICLE THAT IS IN THE UTILITY CLASS.
 
 local currentlyTowedVehicle = nil
 
@@ -6,27 +17,45 @@ RegisterCommand("tow", function()
 	TriggerEvent("tow")
 end,false)
 
+function isVehicleATowTruck(vehicle)
+    local isValid = false
+    for k,model in ipairs(allowedTowModels) do
+        if IsVehicleModel(vehicle, model) then
+            isValid = true
+            break
+        end
+    end
+    return isValid
+end
+
+function isTargetVehicleATrailer(modelHash)
+    if GetVehicleClassFromName(modelHash) == 11 then
+        return true
+    else
+        return false
+    end
+end
+
 RegisterNetEvent('tow')
 AddEventHandler('tow', function()
 	
 	local playerped = PlayerPedId()
 	local vehicle = GetVehiclePedIsIn(playerped, true)
 	
-	local towmodel = GetHashKey('flatbed2')
-	local isVehicleTow = IsVehicleModel(vehicle, towmodel)
+	local isVehicleTow = isVehicleATowTruck(vehicle)
 
 	if isVehicleTow then
 
 		local coordA = GetEntityCoords(playerped, 1)
 		local coordB = GetOffsetFromEntityInWorldCoords(playerped, 0.0, 5.0, 0.0)
 		local targetVehicle = getVehicleInDirection(coordA, coordB)
+        
 
 		Citizen.CreateThread(function()
 			while true do
 				Citizen.Wait(0)
-				local towmodel = GetHashKey('flatbed2')
-				local isVehicleTow = IsVehicleModel(vehicle, towmodel)
-				roll = GetEntityRoll(GetVehiclePedIsIn(PlayerPedId(), true))
+				isVehicleTow = isVehicleATowTruck(vehicle)
+				local roll = GetEntityRoll(GetVehiclePedIsIn(PlayerPedId(), true))
 				if IsEntityUpsidedown(GetVehiclePedIsIn(PlayerPedId(), true)) and isVehicleTow or roll > 70.0 or roll < -70.0 then
 					DetachEntity(currentlyTowedVehicle, false, false)
 					currentlyTowedVehicle = nil
@@ -37,25 +66,37 @@ AddEventHandler('tow', function()
 
 		if currentlyTowedVehicle == nil then
 			if targetVehicle ~= 0 then
-				if not IsPedInAnyVehicle(playerped, true) then
-					if vehicle ~= targetVehicle and IsVehicleStopped(vehicle) then
-						AttachEntityToEntity(targetVehicle, vehicle, GetEntityBoneIndexByName(vehicle, 'bodyshell'), 0, -1.5, 0.5, 0, 0, 0, 1, 1, 0, 1, 0, 1)
-						currentlyTowedVehicle = targetVehicle
-						ShowNotification("~o~~h~Tow Service:~n~~s~Vehicle has been loaded onto the flatbed.")
-						else
-						ShowNotification("~o~~h~Tow Service:~n~~s~There is currently no vehicle on the flatbed.")
-					end
-				end
+                local targetModelHash = GetEntityModel(targetVehicle)
+                -- Check to make sure the target vehicle is allowed to be towed (see settings at lines 8-12)
+                if not ((not allowTowingBoats and IsThisModelABoat(targetModelHash)) or (not allowTowingHelicopters and IsThisModelAHeli(targetModelHash)) or (not allowTowingPlanes and IsThisModelAPlane(targetModelHash)) or (not allowTowingTrains and IsThisModelATrain(targetModelHash)) or (not allowTowingTrailers and isTargetVehicleATrailer(targetModelHash))) then 
+                    if not IsPedInAnyVehicle(playerped, true) then
+                        if vehicle ~= targetVehicle and IsVehicleStopped(vehicle) then
+                            AttachEntityToEntity(targetVehicle, vehicle, GetEntityBoneIndexByName(vehicle, 'bodyshell'), 0, -1.5, 0.5, 0, 0, 0, 1, 1, 0, 1, 0, 1)
+                            currentlyTowedVehicle = targetVehicle
+                            ShowNotification("~o~~h~Tow Service:~n~~s~Vehicle has been loaded onto the flatbed.")
+                        else
+                            ShowNotification("~o~~h~Tow Service:~n~~s~There is currently no vehicle on the flatbed.")
+                        end
+                    else
+                        ShowNotification("~o~~h~Tow Service:~n~~s~You need to be outside of your vehicle to load or unload vehicles.")
+                    end
+                else
+                    ShowNotification("~o~~h~Tow Service:~n~~s~Your towtruck is not equipped to tow this vehicle.")
+                end
+            else
+                ShowNotification("~o~~h~Tow Service:~n~~s~No towable vehicle detected.")
 			end
 		elseif IsVehicleStopped(vehicle) then
-		DetachEntity(currentlyTowedVehicle, false, false)
-		local vehiclesCoords = GetOffsetFromEntityInWorldCoords(vehicle, 0.0, -8.0, 0.0)
+            DetachEntity(currentlyTowedVehicle, false, false)
+            local vehiclesCoords = GetOffsetFromEntityInWorldCoords(vehicle, 0.0, -8.0, 0.0)
 			SetEntityCoords(currentlyTowedVehicle, vehiclesCoords["x"], vehiclesCoords["y"], vehiclesCoords["z"], 1, 0, 0, 1)
 			SetVehicleOnGroundProperly(currentlyTowedVehicle)
 			currentlyTowedVehicle = nil
 			ShowNotification("~o~~h~Tow Service:~n~~s~Vehicle has been unloaded from the flatbed.")
 		end
-	end
+	else
+        ShowNotification("~o~~h~Tow Service:~n~~s~Your vehicle is not registered as an official ~o~Tow Service ~s~tow truck.")
+    end
 end)
 
 function getVehicleInDirection(coordFrom, coordTo)
@@ -66,6 +107,6 @@ end
 
 function ShowNotification(text)
 	SetNotificationTextEntry("STRING")
-	AddTextComponentString(text)
+    AddTextComponentSubstringPlayerName(text)
 	DrawNotification(false, false)
 end
